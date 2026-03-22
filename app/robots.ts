dc7179f2717dea5fe520340d364b19bce31e1800
likeknightking <likeknightking@gmail.com>
2026-03-22 17:16:30 -0300

    Restore www in URLs (Vercel redirects non-www to www)

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 export default function robots() {
   return {
     rules: { userAgent: '*', allow: '/' },
-    sitemap: 'https://formatmyjson.com/sitemap.xml',
+    sitemap: 'https://www.formatmyjson.com/sitemap.xml',
   }
 }
